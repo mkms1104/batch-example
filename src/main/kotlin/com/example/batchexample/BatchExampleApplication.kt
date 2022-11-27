42b0c0bd41ad8934c6af50bbbeb95e1da7f0253b
mskim <mskim@11h11m.com>
2022-11-27 15:29:38 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/batchexample/BatchExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.batchexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BatchExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<BatchExampleApplication>(*args)
+}
